df0d30374c71d9142ff2862bf8972ccb0ebfb2be
vitalikdanchenko <devyjones@gmail.com>
2016-07-19 17:27:49 +0300

    fix potential bug

--- lib/talkable/api/offer.rb
+++ lib/talkable/api/offer.rb
@@ -2,7 +2,7 @@ module Talkable
   module API
     class Offer < Base
       class << self
-        def find(id, params = nil)
+        def find(id, params = {})
           get "/offers/#{id}", params
         end
       end
