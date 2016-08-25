5540c2736d6e307f62b9dcfbe4f914cce0ccc163
vitalikdanchenko <devyjones@gmail.com>
2016-08-25 15:12:36 +0300

    freezing constants

--- lib/talkable/api.rb
+++ lib/talkable/api.rb
@@ -5,7 +5,7 @@ require 'talkable/api/visitor'
 
 module Talkable
   module API
-    VERSION = "v2"
+    VERSION = "v2".freeze
     class BadRequest < StandardError
     end
     class NetworkError < StandardError
--- lib/talkable/version.rb
+++ lib/talkable/version.rb
@@ -1,3 +1,3 @@
 module Talkable
-  VERSION = "0.0.0"
+  VERSION = "0.0.0".freeze
 end
