05372c897e69b29e2a09fa1a47dcc292100c8a7a
Anton Paisov <paisov@gmail.com>
2018-04-20 00:01:07 +0300

    [PR-8978] Fixed install generator (#18)
    And Updated fixtures to match corresponding files from a fresh Rails 5.2
    project.

--- spec/fixtures/files/routes.rb
+++ spec/fixtures/files/routes.rb
@@ -1,4 +1,3 @@
 Rails.application.routes.draw do
-  # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
